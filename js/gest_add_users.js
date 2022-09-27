3d71d98a3a61cce8363cb1f9cd30c059c2b33447
Carlos Alberto Vivas Reinoso <carlos_vivas2021@outlook.com>
2022-09-26 21:06:44 -0500

    se agregan las validaciones al formulario de insertar o editar registro

--- js/gest_add_users.js
+++ js/gest_add_users.js
@@ -2,17 +2,25 @@
 
 //Me permite realizar acciones inmediatamente despues de cargar la pagina.
 document.addEventListener("DOMContentLoaded", () => {
+  
   cargarSelects();
+  
+  let txtId = document.getElementById("txtId")
   const EditarId = sessionStorage.getItem("EditarId");
-  console.log(EditarId)
+ 
   if (EditarId != "" && EditarId != null ) {
+    txtId.value = EditarId
     cargarDatosUsuario(EditarId);
+  }
+  else
+  {
+    txtId.value = 0;
   }
 
   const nombre = sessionStorage.getItem("nombreUsuario");
   const perfil = sessionStorage.getItem("perfil");
   const nombrePerfil = sessionStorage.getItem("nombrePerfil");
-  console.log(nombre);
+  
   if (nombre == null) {
     mensaje(
       "Mensaje del sistema",
@@ -27,9 +35,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //carga los selects del formulario
-function cargarSelects() {
-  CargarPerfiles();
-  CargarTiposDocumentos();
+async function  cargarSelects() {
+  await CargarPerfiles();
+  await CargarTiposDocumentos();
 }
 
 //cargar los datos del usuario a editar
@@ -43,35 +51,20 @@ const cargarDatosUsuario = async (id) => {
   if (data.ok) {
     const djson = await data.json();
     const ojson = JSON.parse(JSON.stringify(djson));
-    console.log(ojson);
     
     ojson.forEach((usuario) => {
-        let txtId = document.getElementById("txtId");
-        txtId.value = id;
 
-        let txtUsuario = document.getElementById("txtUsuario");
-        txtUsuario.value = usuario.login;
-
-      let txtClave = document.getElementById("txtClave");
-      txtClave.value = usuario.password;
-
-      let selPerfil = document.getElementById("selPerfil");
-      selPerfil.value = usuario.perfilesId;
-
-      let selTipoDocumento = document.getElementById("selTipoDocumento");
-      selTipoDocumento.value = usuario.tiposdocumentosId;
-
-      let txtNumero = document.getElementById("txtNumero");
-      txtNumero.value = usuario.identificacion;
-
-      let txtNombres = document.getElementById("txtNombres")
-      txtNombres.value = usuario.nombres;
-
-      let txtPrimerApellido = document.getElementById("txtPrimerApellido");
-      txtPrimerApellido.value = usuario.primerapellido;
-
-      let txtSegundoApellido = document.getElementById("txtSegundoApellido");
-      txtSegundoApellido.value = usuario.segundoapellido;
+        let registro = getControles();
+        
+        registro.txtId.value = id;
+        registro.txtUsuario.value = usuario.login;
+        registro.txtClave.value = usuario.password;
+        registro.selPerfil.value = usuario.perfilesId;
+        registro.selTipoDocumento.value = usuario.tiposdocumentosId;
+        registro.txtNumero.value = usuario.identificacion;
+        registro.txtNombres.value = usuario.nombres;
+        registro.txtPrimerApellido.value = usuario.primerapellido;
+        registro.txtSegundoApellido.value = usuario.segundoapellido;
 
     });
   } else {
@@ -92,7 +85,6 @@ const CargarPerfiles = async () => {
   if (data.ok) {
     const djson = await data.json();
     const ojson = JSON.parse(JSON.stringify(djson));
-    console.log(ojson);
     let cadena = "<option selected>Seleccione...</option>";
     ojson.forEach((perfil) => {
       cadena += `<option value=${perfil.id}>${perfil.descripcion}</option>`;
@@ -103,7 +95,6 @@ const CargarPerfiles = async () => {
     alert("Error al consumir servicio de perfiles.");
   }
 
-  console.log(data);
 };
 
 //consulta los datos de la base de datos de tipos de documento y los coloca en el select.
@@ -119,7 +110,7 @@ const CargarTiposDocumentos = async () => {
   if (data.ok) {
     const djson = await data.json();
     const ojson = JSON.parse(JSON.stringify(djson));
-    console.log(ojson);
+
     let cadena = "<option selected>Seleccione...</option>";
     ojson.forEach((tipo) => {
       cadena += `<option value=${tipo.id}>${tipo.descripcion}</option>`;
@@ -130,5 +121,120 @@ const CargarTiposDocumentos = async () => {
     alert("Error al consumir servicio de perfiles.");
   }
 
-  console.log(data);
+
 };
+
+//guardar registros segun sea editar o insertar.
+function guardarRegistro(){
+
+  if(validarDatos())
+  {
+    const registro = getControles();
+
+    //se inserta nuevo registro
+    if(registro.txtId.value=="0")
+    {
+      console.log("insertar registro")
+    }
+    else //se actualiza registro
+    {
+      console.log("actualiza registro")
+    }
+    
+
+  }
+
+}
+
+//valida los datos que ingresó el usuarios
+function validarDatos(){
+
+  registro = getControles();
+  let encabezado = "Se generaron los siguientes errores:";
+  let cadena = "";
+
+  if (txtUsuario.value=="")
+  {
+    cadena += "<br> El campo <strong>Usuario</strong> es obligatorio"
+  }
+
+  if (txtClave.value=="")
+  {
+    cadena += "<br> El campo <strong>Clave</strong> es obligatorio "
+  }
+
+  if (selPerfil.value=="Seleccione...")
+  {
+    cadena += "<br> El campo <strong>Perfil</strong> es obligatorio "
+  }
+
+  if (selTipoDocumento.value=="Seleccione...")
+  {
+    cadena += "<br> El campo <strong>Tipo de Documento</strong> es obligatorio "
+  }
+
+  if (txtNumero.value=="")
+  {
+    cadena += "<br> El campo <strong>Numero de Documento</strong> es obligatorio "
+  }
+  
+  if (txtNombres.value=="")
+  {
+    cadena += "<br> El campo <strong>Nombres</strong> es obligatorio "
+  }
+
+  if (txtPrimerApellido.value=="")
+  {
+    cadena += "<br> El campo <strong>Primer Apellido</strong> es obligatorio "
+  }
+
+  if (cadena != "")
+  {
+    cadena = encabezado + cadena
+    showWarning(cadena)
+    return false
+  }
+  
+  return true;
+
+}
+
+//guarda los datos de los controles en una estructura
+function getControles()
+{
+  const data  =
+  {
+    txtId : document.getElementById("txtId"),
+    txtUsuario : document.getElementById("txtUsuario"),
+    txtClave : document.getElementById("txtClave"),
+    selPerfil : document.getElementById("selPerfil"),
+    selTipoDocumento : document.getElementById("selTipoDocumento"),
+    txtNumero : document.getElementById("txtNumero"),
+    txtNombres : document.getElementById("txtNombres"),
+    txtPrimerApellido : document.getElementById("txtPrimerApellido"),
+    txtSegundoApellido : document.getElementById("txtSegundoApellido")
+  }
+
+  return data; 
+
+}
+
+//mostrar mensajes de error
+const showError = (message) => {
+  alert(message, "danger");
+}
+
+//mostrar mensajes de advertencia
+const showWarning = (message) => {
+  alert(message, "warning");
+}
+
+//funcion para construir el mensaje a enviar al usuario.
+const alert = (message, type) => {
+  document.getElementById("errorBox").innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+}
